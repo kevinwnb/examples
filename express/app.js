127454bdbc951b3140a15587c0e24545ff377d6a
Kevin Wegener Antolinez <kevinwnb@gmail.com>
2022-09-22 18:51:24 +0200

    update

--- express/app.js
+++ express/app.js
@@ -50,15 +50,18 @@ const express = require("express")
 
 const app = express()
 
-app.get("/",(req, res)=>{
+
+app.get("/api/one", (req, res) => {
     console.log(req.socket.remoteAddress)
     res.status(200).send(req.socket.remoteAddress)
 })
+
+app.use(express.static("./public"))
 
 app.all("*", (req, res) => {
     res.status(404).send("Resource not found")
 })
 
-app.listen(5000, ()=>{
+app.listen(5000, () => {
     console.log("Server is running on port 5000...")
 })
