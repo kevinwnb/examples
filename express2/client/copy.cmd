1f1c047b8843febc0be4ad0869516fa1936b732e
Kevin Wegener Antolinez <kevinwnb@gmail.com>
2022-10-07 22:18:25 +0200

    update

--- express2/client/copy.cmd
+++
@@ -1,2 +0,0 @@
-xcopy C:\Users\kevin\Documents\GitHub\examples\express2\client\build C:\Users\kevin\Documents\GitHub\examples\express2\public /Y /E
-exit
--- express2/products.js
+++
@@ -1,8 +0,0 @@
-let products = [
-    { id: 1, name: "Long sleeve pullover" },
-    { id: 2, name: "T-Shirt" },
-    { id: 3, name: "Pants" },
-    { id: 4, name: "Socks" },
-]
-
-module.exports = products
