9eb8b196f915a0a0cd3d6cfbbba96d298b3d0b4b
Kevin Wegener Antolinez <kevinwnb@gmail.com>
2022-10-12 20:04:39 +0200

    Update Home.js

--- express2/client/src/component/Home.js
+++ express2/client/src/component/Home.js
@@ -5,7 +5,7 @@ function Home(props) {
     props.setActiveLink("home")
     return (
         <div className="container">
-<h2 className="text-center mb-5">Home</h2>
+<h2 className="text-center mb-5">Home a</h2>
             </div>
     )
 }
